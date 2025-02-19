5b682609e5ea2a32cd3fb83cc591b7bab253aa07
Angie Roa <a.roap@uniandes.edu.co>
2025-02-18 19:19:49 -0500

    feat: agregar microservicio

--- src/purchases/app.py
+++ src/purchases/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-
+import random
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "hello world"
+@app.route('/ping')
+def health_check():
+    if random.random() < 0.9:
+        return {"status": "echo"}, 200
+    return {"status": "INTERNAL SERVER ERROR"}, 500
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5002)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
